37332166851a7cebf2274231d46d08fcf6164bec
Oryam <48219965+or-yam@users.noreply.github.com>
2020-06-25 12:54:05 +0300

    init twitter app

---
+++ logic.js
@@ -0,0 +1,114 @@
+const TwitterModule = () => {
+  let _posts = [
+    {
+      text: 'First post!',
+      id: 'p1',
+      comments: [
+        { id: 'c1', text: 'First comment on first post!' },
+        { id: 'c2', text: 'Second comment on first post!!' },
+        { id: 'c3', text: 'Third comment on first post!!!' },
+      ],
+    },
+    {
+      text: 'Aw man, I wanted to be first',
+      id: 'p2',
+      comments: [
+        {
+          id: 'c4',
+          text: "Don't wory second poster, you'll be first one day.",
+        },
+        { id: 'c5', text: 'Yeah, believe in yourself!' },
+        { id: 'c6', text: 'Haha second place what a joke.' },
+      ],
+    },
+  ];
+
+  let lastPostId = 'p2';
+  let lastCommentId = 'c6';
+
+  const createNewPostId = () => {
+    lastPostId = `p${parseInt(lastPostId[1]) + 1}`;
+    return lastPostId;
+  };
+  const createNewCommentId = () => {
+    lastCommentId = `c${parseInt(lastCommentId[1]) + 1}`;
+    return lastPostId;
+  };
+
+  const postIdCounter = () => {
+    return _posts.length;
+  };
+  const commentIdCounter = () => {
+    let counter = 0;
+    for (const post of _posts) {
+      counter += post.comments.length;
+    }
+    return counter;
+  };
+  const getPosts = () => {
+    return _posts;
+  };
+  const addPost = (text) => {
+    newId = createNewPostId();
+    newPost = {
+      text: text,
+      id: newId,
+      comments: [],
+    };
+    _posts.push(newPost);
+  };
+  const removePost = (postId) => {
+    for (const post of _posts) {
+      if (post.id === postId) {
+        let index = _posts.indexOf(post);
+        _posts.splice(index, 1);
+        break;
+      }
+    }
+  };
+
+  const addComment = (text, postId) => {
+    for (const post of _posts) {
+      if (post.id === postId) {
+        newId = createNewCommentId();
+        newComment = {
+          id: newId,
+          text: text,
+        };
+        post.comments.push(newComment);
+        break;
+      }
+    }
+  };
+
+  ////to much repete code with add and remove post/comment
+  ////can make an array of id's and avoid nested loops
+
+  const removeComment = (postId, commentId) => {
+    for (const post of _posts) {
+      if (post.id === postId) {
+        for (const comment of post.comments) {
+          if (comment.id === commentId) {
+            let index = post.comments.indexOf(comment);
+            post.comments.splice(index, 1);
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  return {
+    postIdCounter: postIdCounter,
+    commentIdCounter: commentIdCounter,
+    getPosts: getPosts,
+    addPost: addPost,
+    removePost: removePost,
+    addComment: addComment,
+    removeComment: removeComment,
+  };
+};
+
+
+
+
---
+++ render.js
@@ -0,0 +1,31 @@
+const Renderer = () => {
+  const renderPosts = (posts) => {
+    $('#posts').empty();
+    for (const post of posts) {
+      let postText = post.text;
+      let postId = post.id;
+      let comments = post.comments;
+      let postTemplate = `<div class="post" data-id=${postId}>${postText}
+      <div class="post-text" </div>
+      <div class="comments"></div>
+      <input class="comment-input" type="text" />
+      <button class="comment-btn">Comment</button>
+      <button class="delete">Delete Post</button>
+    </div>`;
+
+      $('#posts').append(postTemplate);
+
+      for (const comment of comments) {
+        let commentId = comment.id;
+        let commentText = comment.text;
+        let commentTemplate = `<div class="comment" data-id=${comment.id}>
+        <span class="delete-comment">X</span>
+        <span class="comment-text">${comment.text}</span>
+      </div>`;
+        $(`[data-id=${postId}]`).find('.comments').append(commentTemplate);
+      }
+    }
+  };
+
+  return { renderPosts };
+};
